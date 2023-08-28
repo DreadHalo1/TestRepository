fa7208258a303439b494a41c99dfc4875018ef4c
Dread Halo <nikitanovosad763@gmail.com>
2023-08-28 17:31:57 +0300

    added form with Vigenere cipher

---
+++ TestForm/TestForm/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShowTextWindow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Text = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.convert = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // ShowTextWindow
+            // 
+            this.ShowTextWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ShowTextWindow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ShowTextWindow.Location = new System.Drawing.Point(33, 432);
+            this.ShowTextWindow.Margin = new System.Windows.Forms.Padding(0);
+            this.ShowTextWindow.Name = "ShowTextWindow";
+            this.ShowTextWindow.Size = new System.Drawing.Size(267, 79);
+            this.ShowTextWindow.TabIndex = 0;
+            this.ShowTextWindow.Text = "Действие";
+            this.ShowTextWindow.UseVisualStyleBackColor = true;
+            this.ShowTextWindow.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(235, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Шифр Виженера";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Text
+            // 
+            this.Text.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Text.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F);
+            this.Text.Location = new System.Drawing.Point(200, 119);
+            this.Text.Margin = new System.Windows.Forms.Padding(4);
+            this.Text.Multiline = true;
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(743, 74);
+            this.Text.TabIndex = 2;
+            this.Text.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(29, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите текст";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F);
+            this.label3.Location = new System.Drawing.Point(29, 238);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Вверите пароль";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F);
+            this.label4.Location = new System.Drawing.Point(27, 349);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Зашыфрованый вид";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // password
+            // 
+            this.password.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.password.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F);
+            this.password.Location = new System.Drawing.Point(200, 238);
+            this.password.Margin = new System.Windows.Forms.Padding(4);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(743, 29);
+            this.password.TabIndex = 6;
+            this.password.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // convert
+            // 
+            this.convert.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.convert.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.convert.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F);
+            this.convert.HideSelection = false;
+            this.convert.Location = new System.Drawing.Point(343, 331);
+            this.convert.Margin = new System.Windows.Forms.Padding(4);
+            this.convert.Multiline = true;
+            this.convert.Name = "convert";
+            this.convert.ReadOnly = true;
+            this.convert.Size = new System.Drawing.Size(600, 74);
+            this.convert.TabIndex = 7;
+            this.convert.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(603, 458);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(119, 20);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "зашыфровать";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(728, 458);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 20);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "расшыфровать";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 558);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ShowTextWindow);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ShowTextWindow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox convert;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ TestForm/TestForm/Vigenere.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TEST
+{
+    internal class Vigenere
+    {
+        List<char> alphabet;
+
+        public string VigenereG(string password, string text, bool decode)
+        {
+            password = password.ToLower();
+            text = text.ToLower();
+
+            string tempText = "";
+            int tempValue = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (alphabet.Contains(text[i]) != true)
+                {
+                    tempText += text[i];
+                }
+                else
+                {
+                    if (decode != true)
+                    {
+                        tempText += alphabet[(alphabet.IndexOf(text[i]) + alphabet.IndexOf(password[tempValue])) % alphabet.Count];
+                    }
+                    else
+                    {
+                        int temp = alphabet.IndexOf(text[i]) - alphabet.IndexOf(password[tempValue]);
+                        tempText += temp >= 0 ? alphabet[temp % alphabet.Count] : alphabet[temp + alphabet.Count];
+                    }
+
+                    if (tempValue + 1 > password.Length - 1)
+                        tempValue = 0;
+                    else
+                        tempValue++;
+                }
+            }
+            return tempText;
+        }
+        public Vigenere(string alphabet)
+        {
+            this.alphabet = alphabet.ToLower().ToCharArray().ToList();
+        }
+    }
+}
